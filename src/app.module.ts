785caf3cc6d3a1ebe6f0c5f34daa4cfff08b85ac
Dima Danilov <nervogryz@gmail.com>
2024-06-14 02:12:05 +0300

    fix imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,12 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProductModule } from './product/product.module';
-import { WhmanModule } from './whman/whman.module';
 import { WhModule } from './wh/wh.module';
 import { CategoryModule } from './category/category.module';
 import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
-    WhmanModule,
     AuthModule,
     ProductModule,
     WhModule,
